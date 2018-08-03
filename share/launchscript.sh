0dd84ecb9839494a6d59cc36860da5d923bbf3a7
Aaron Virshup <avirshup@gmail.com>
2018-08-03 13:27:38 -0700

    Set up inputrc for bash

--- share/launchscript.sh
+++ share/launchscript.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 initfile=/tmp/dutil/shellinit
+inputrcfile=/tmp/dutil/inputrc
 
 if (command -v bash >/dev/null 2>&1) then
     export SHELL=bash
+    if [ -z $INPUTRC ] && [ ! -f $HOME/.inputrc ]; then
+        export INPUTRC=${inputrcfile}
+    fi
+
     bash --init-file ${initfile}
 else
     if [ -z "${ENV}" ]; then
